80e52662ab8f037f0a1105cc21bd00e27a24614a
genrum <gen.rumtcv@gmail.com>
2022-10-10 12:11:27 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+/* let admin;
+let name;
+name = "john";
+admin = name;
+console.log(admin);
+let ourPlanetName = "earth";
+let currentUserName = "dimas";
+console.log(`hello me fellow ${"w"+"a"}`); */
+/* let a = 1, b = 1; // a = 2, b = 2
+
+let c = ++a; // 2
+let d = b++; // 1
+console.log(a,b,c,d); */
+
+/* let a = 2; 
+
+let x = 1 + (a *= 2); // a = 4, x = 5;
+console.log(a,x); */
+
+/* "" + 1 + 0  // "10"
+"" - 1 + 0  //  -1
+true + false // 1
+6 / "3" // 2
+"2" * "3" // 6
+4 + 5 + "px" // "9px"
+"$" + 4 + 5    "$45"
+"4" - 2        2
+"4px" - 2       NaN
+"  -9  " + 5     "  -9  5"
+"  -9  " - 5    -14
+null + 1         1
+undefined + 1    NaN
+" \t \n" - 2     -2 */
+let ff = "4";
+console.log("\t \n" + 2);
