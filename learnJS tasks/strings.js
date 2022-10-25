d41c5cfdc205a0358afca34cff2174f1a1ecdb7c
Gennadii Rumiantcev <gen.rumtcv@gmail.com>
2022-10-25 15:54:02 -0700

    removed function return, now it console.logs

--- learnJS tasks/strings.js
+++ learnJS tasks/strings.js
@@ -1,29 +1,3 @@
-function ucFirst(str) {
-    if(!str) return str;
-    return str[0].toUpperCase() + str.slice(1);
-}
-
-function checkSpam(str) {
-    let flagWord1 = "viagra";
-    let flagWord2 = "xxx";
-    let lowStr = str.toLowerCase()
-    return lowStr.includes(flagWord1) || lowStr.includes(flagWord2);
-
-}
-
-function truncate(str, maxlength) {
-    if (str.length > maxlength) {
-        return str.slice(0, maxlength-1) + "..."
-    } else return str;
-}
-
-
-function exTheMoney(str) {
-    let valueStr = str.slice(1);
-    return Number(valueStr)
-} 
-
-
 function showVerticalMessage(str) {
     let vertStr = "";
     str = (str[0] === "s") ? str[0].toUpperCase() + str.slice(1) : str;
@@ -31,7 +5,7 @@ function showVerticalMessage(str) {
     for (let i=0; i < maxLength; i++) {
         vertStr += str[i]+"\n";
     }
-    return vertStr
+    console.log(vertStr)
 }
 
-console.log(showVerticalMessage("v23456789"));
+showVerticalMessage("");
